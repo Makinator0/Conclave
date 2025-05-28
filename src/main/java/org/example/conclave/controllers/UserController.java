317b73ae7296dc779ea92d8fab6ecbf3797e690b
vkukurika <vkukurika@nymbus.com>
2025-05-29 00:39:17 +0300

    Updated UserController

--- src/main/java/org/example/conclave/controllers/UserController.java
+++ src/main/java/org/example/conclave/controllers/UserController.java
@@ -18,7 +18,8 @@ public class UserController {
     private final UserService userService;
     private final AuthService authService;
     private final RegistrationService registrationService;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public UserController(UserService userService, AuthService authService, RegistrationService registrationService) {
         this.userService = userService;
         this.authService = authService;
@@ -30,6 +31,10 @@ public class UserController {
             User user = (User) userService.loadUserByUsername(loginRequest.getUsername());
             System.out.println(user.getPassword().toString());
             System.out.println(user.getUsername().toString());
+            // Проверка пароля
+            if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Неверный логин или пароль");
+            }
             String token = authService.generateAndSetToken(user);
             return ResponseEntity.ok().header("Authorization", "Bearer " + token).body("Успешный вход");
         } catch (Exception e) {
