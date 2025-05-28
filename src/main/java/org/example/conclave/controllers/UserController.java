fd1356864b2f25b9ac7abbb76f4f3c53fb300c5f
vkukurika <vkukurika@nymbus.com>
2025-05-29 00:41:54 +0300

    Updated UserController

--- src/main/java/org/example/conclave/controllers/UserController.java
+++ src/main/java/org/example/conclave/controllers/UserController.java
@@ -8,6 +8,8 @@ import org.example.conclave.services.AuthService;
 import org.example.conclave.services.RegistrationService;
 import org.example.conclave.services.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
